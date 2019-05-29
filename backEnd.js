b80a3cecf48b3a3de3f967b55db2df36e2b17595
MiguelOcegueraM <miguelalejandro_oceguera@ucol.mx>
2019-05-28 22:16:47 -0500

    Cambios realizado, solucion de problema en null

--- backEnd.js
+++ backEnd.js
@@ -1,36 +1,36 @@
-var x1 = parseInt(document.getElementById("x1").value);
-var x2 = parseInt(document.getElementById("x2").value);
-var x3 = parseInt(document.getElementById("x3").value);
-var y1 = parseInt(document.getElementById("y1").value);
-var y2 = parseInt(document.getElementById("y2").value);
-var y3 = parseInt(document.getElementById("y3").value);
-var z1 = parseInt(document.getElementById("z1").value);
-var z2 = parseInt(document.getElementById("z2").value);
-var z3 = parseInt(document.getElementById("z3").value);
-var r1 = parseInt(document.getElementById("r1").value);
-var r2 = parseInt(document.getElementById("r2").value);
-var r3 = parseInt(document.getElementById("r3").value);
+var x1 = parseInt(document.getElementById("x1") || {}).value;
+var x2 = parseInt(document.getElementById("x2") || {}).value;
+var x3 = parseInt(document.getElementById("x3") || {}).value;
+var y1 = parseInt(document.getElementById("y1") || {}).value;
+var y2 = parseInt(document.getElementById("y2") || {}).value;
+var y3 = parseInt(document.getElementById("y3") || {}).value;
+var z1 = parseInt(document.getElementById("z1") || {}).value;
+var z2 = parseInt(document.getElementById("z2") || {}).value;
+var z3 = parseInt(document.getElementById("z3") || {}).value;
+var r1 = parseInt(document.getElementById("r1") || {}).value;
+var r2 = parseInt(document.getElementById("r2") || {}).value;
+var r3 = parseInt(document.getElementById("r3") || {}).value;
 var resultado = 0;
 
 function determinanteFunc(x1, x2, x3, y1, y2, y3, z1, z2, z3) {
-    determinante = ((x1 * y2 * z3) + (x2 * y3 * z1) + (x3 * y1 * z2)) - ((x2 * y1 * z3) + (x1 * y3 * z2) + (x3 * y2 * z1)); //Determinante
+    var determinante = ((x1 * y2 * z3) + (x2 * y3 * z1) + (x3 * y1 * z2)) - ((x2 * y1 * z3) + (x1 * y3 * z2) + (x3 * y2 * z1)); //Determinante
     return determinante;
 }
 
 function resultadoX(y1, y2, y3, z1, z2, z3, r1, r2, r3) {
-    resultado = ((r1 * y2 * z3) + (r2 * y3 * z1) + (r3 * y1 * z2)) - ((r2 * y1 * z3) + (r1 * y3 * z2) + (r3 * y2 * z1));
-    resultadoX = resultado / determinanteFunc;
+    var resultadoofX = ((r1 * y2 * z3) + (r2 * y3 * z1) + (r3 * y1 * z2)) - ((r2 * y1 * z3) + (r1 * y3 * z2) + (r3 * y2 * z1));
+    resultadoX = resultadoofX / determinanteFunc;
     return resultadoX;
 }
 
 function resultadoY(x1, x2, x3, z1, z2, z3, r1, r2, r3) {
-    resultado = ((x1 * r2 * z3) + (x2 * r3 * z1) + (x3 * r1 * z2)) - ((x2 * r1 * z3) + (x1 * r3 * z2) + (x3 * r2 * z1));
-    resultadoY = resultado / determinanteFunc;
+    var resultadoofY = ((x1 * r2 * z3) + (x2 * r3 * z1) + (x3 * r1 * z2)) - ((x2 * r1 * z3) + (x1 * r3 * z2) + (x3 * r2 * z1));
+    resultadoY = resultadoofY / determinanteFunc;
     return resultadoY;
 }
 
 function resultadoZ(x1, x2, x3, y1, y2, y3, r1, r2, r3) {
-    resultado = ((x1 * y2 * r3) + (x2 * y3 * r1) + (x3 * y1 * r2)) - ((x2 * y1 * r3) + (x1 * y3 * r2) + (x3 * y2 * r1));
+    var resultadoofZ = ((x1 * y2 * r3) + (x2 * y3 * r1) + (x3 * y1 * r2)) - ((x2 * y1 * r3) + (x1 * y3 * r2) + (x3 * y2 * r1));
     resultadoZ = resultado / determinanteFunc;
     return resultadoZ;
 }
